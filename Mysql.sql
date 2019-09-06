90b4b8a62db65a025d4311f94fff0cd1c73dc04a
guptaneha346 <neha.gupta@impressico.com>
2019-09-06 17:52:45 +0530

    initial commit

---
+++ Mysql.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE feedback ; 
+CREATE TABLE feedbacktable ( id INT NOT NULL AUTO_INCREMENT, subject VARCHAR(30) NOT NULL, creater VARCHAR(30) NOT NULL, traineename VARCHAR(50), empcode VARCHAR(50), rating1 VARCHAR(50), rating2 VARCHAR(50), rating3 VARCHAR(50), rating4 VARCHAR(50), rating5 VARCHAR(50), suggestion1 VARCHAR(50),suggestion2 VARCHAR(50),suggestion3 VARCHAR(50), PRIMARY KEY ( id ) );
