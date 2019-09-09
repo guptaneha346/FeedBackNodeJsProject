5725f8fc804d905d69c539fdcf1a61ad335bc017
guptaneha346 <neha.gupta@impressico.com>
2019-09-09 11:16:21 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,29 +2,32 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var sqlQuery = require('./MySqlConnection');
 const Joi = require('joi');
-const expressJoi = require('express-joi-validator');
+
+const validator = require('express-joi-validation').createValidator({
+
+})
 var app = express();
 const PORT = 8087;
 
-const schema = {
-
-    id: Joi.number().required()
-
-}
-
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('../library'));
-app.set('view engine');
 
 app.get('/', function (req, res) {
     res.sendFile('FeedBackPage.html', { root: __dirname })
 });
 
-app.post('/submit', function (req, res) {
+    const querySchema = Joi.object({
+    traineename: Joi.string().required(),
+    empcode: Joi.string().required(),
+    rating1: Joi.string().required(),
+    rating2: Joi.string().required(),
+    rating3: Joi.string().required(),
+    rating4: Joi.string().required(),
+    rating5: Joi.string().required()
+})
+
+app.post('/submit', validator.query(querySchema), function (req, res) {
     var sql = "insert into feedbacktable values(null,'" + req.body.subject + "','"
         + req.body.creater + "','" + req.body.traineename + "','" + req.body.empcode +
         "','" + req.body.rating1 + "','" + req.body.rating2 + "','" + req.body.rating3 +
@@ -50,12 +53,19 @@ app.get('/response', function (req, res) {
 
     sqlQuery.executeQueryWithCallback(sql, function (results) {
         res.writeHeader(200, { "Content-Type": "text/html" });
-        res.write("<table border='1'><tr><th>Trainee Name</th><th> Employee Code</th><th>The state objective of the training were met</th></tr>");
+        res.write("<table border='1'><tr><th>Trainee Name</th><th> Employee Code</th><th>Rating1</th><th>Rating2</th><th>Rating3</th><th>Rating4</th><th>Rating5</th><th>suggestion1</th><th>suggestion2</th><th>suggestion3</th></tr>");
         for (var index = 0; index < results.length; index++) {
             res.write("<tr>");
             res.write("<td>" + results[index].traineename + "</td>");
             res.write("<td>" + results[index].empcode + "</td>");
             res.write("<td>" + results[index].rating1 + "</td>");
+            res.write("<td>" + results[index].rating2 + "</td>");
+            res.write("<td>" + results[index].rating3 + "</td>");
+            res.write("<td>" + results[index].rating4 + "</td>");
+            res.write("<td>" + results[index].rating5 + "</td>");
+            res.write("<td>" + results[index].suggestion1 + "</td>");
+            res.write("<td>" + results[index].suggestion2 + "</td>");
+            res.write("<td>" + results[index].suggestion3 + "</td>");
             res.write("</tr>");
         }
         res.write("</table>");
